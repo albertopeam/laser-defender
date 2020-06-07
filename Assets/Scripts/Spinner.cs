5fb5c6bba8e0efaa5b2bcc54090d629283cab878
Alberto Penas Amor <alberto.penas@nomasystems.com>
2020-06-07 14:43:42 +0200

    added sorting layer & spinning projectile

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    [SerializeField] float oneRotationEachSeconds = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaRotation = 360 * Time.deltaTime / oneRotationEachSeconds;
+
+        Transform transform = gameObject.transform;
+        float newRotation = transform.rotation.z + deltaRotation;
+
+        transform.Rotate(new Vector3(0, 0, newRotation));
+    }
+}
